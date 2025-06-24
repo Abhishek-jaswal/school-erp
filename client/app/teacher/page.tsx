52e07c00357924dc00c1674210ec11d9d66b116a
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-06-24 17:40:05 +0530

    added teacher component

--- client/app/teacher/page.tsx
+++ client/app/teacher/page.tsx
@@ -1,3 +1,3 @@
-// export default function TeacherDashboard() {
-//   return <h1 className="p-6 text-xl">Welcome Teacher</h1>;
-// }
+export default function TeacherDashboard() {
+  return <h1 className="p-6 text-xl">Welcome Teacher</h1>;
+}
