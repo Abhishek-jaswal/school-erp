b49cc3bc7de79a3169ba3ae2d5c72105590df8ad
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-06-25 12:27:11 +0530

    added bubblesort and binary search

--- client/practical/script.js
+++ client/practical/script.js
@@ -5,7 +5,7 @@ console.log("hellow world"
 function binarySearch(arr, target){
     let low = 0;
     let high = arr.length-1;
-    while(left <= right){
+    while(low <= high){
         let mid =Math.floor((low+high)/2);
         if(arr[mid]===target)return mid;
         else if(arr[mid]<target) low = mid+1;
@@ -13,5 +13,21 @@ function binarySearch(arr, target){
     }
     return -1;
 }
-const nums =[2,5,7,10,12,15,20];
+// find the bubble sort algorithm to sort an array
+function bubbleSort(arr){
+    let n = arr.length;
+    for(let i =0;i<n-1; i++){
+        for(let j=0; j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                //swap
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+const nums =bubbleSort([2,5,7,10,12,15,20]);
+console.log(binarySearch(nums,10));
+
 
